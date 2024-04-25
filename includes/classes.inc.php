c1d183e2656900f7ac8a658c5e4e54add45ebe30
HackerFrogPepe <aaronjesseward@gmail.com>
2024-04-25 14:03:06 +1000

    classes/bookings complete with alternate method to buttons

--- includes/classes.inc.php
+++ includes/classes.inc.php
@@ -2,41 +2,46 @@
 if (isset($_SESSION['user_id']) && !isset($_SESSION['admin'])) {
     echo "Welcome to the member's area, " . htmlspecialchars($_SESSION['username']) . "!";
 ?>
-<?php
+    <h3>Book a class here!</h3>
+
+    <form action="../includes/classes_insert_inc.php" method="post">
+        <input type="text" name="class_id" placeholder="Class Id">
+        <input type="text" name="user_id" placeholder="User Id">
+        <button>Book</button>
+    <?php
 } else {
     echo "Please log in to interact with this feature.";
 }
-?>
-<?php
-echo '<h3>Class List</h3>';
+    ?>
+    <?php
+    echo '<h3>Class List</h3>';
 
-require_once('dbh.inc.php');
-$query = 'SELECT class_id, trainer_first_name, class_name, class_time FROM classlist';
-$rows = $pdo->query($query);
-$result = $rows->fetchAll();
-echo "<table class='table table-striped'>";
-echo "<thead>";
-echo "<tr>";
-echo "<th>Class Id</th>";
-echo "<th>Trainer</th>";
-echo "<th>Class Name</th>";
-echo "<th>Class Time</th>";
-echo "</tr>";
-echo "</thead>";
-foreach ($result as $rows) :
-    echo "<tbody>";
+    require_once('dbh.inc.php');
+    $query = 'SELECT class_id, trainer_first_name, class_name, class_time FROM classlist';
+    $rows = $pdo->query($query);
+    $result = $rows->fetchAll();
+    echo "<table class='table table-striped'>";
+    echo "<thead>";
     echo "<tr>";
-    echo "<td>" . $rows['class_id'] . "</td>";
-    echo "<td>" . $rows['trainer_first_name'] . "</td>";
-    echo "<td>" . $rows['class_name'] . "</td>";
-    echo "<td>" . $rows['class_time'] . "</td>";
+    echo "<th>Class Id</th>";
+    echo "<th>Trainer</th>";
+    echo "<th>Class Name</th>";
+    echo "<th>Class Time</th>";
     echo "</tr>";
-    echo "</tbody>";
-endforeach;
-echo "</table>";
-?>
-<a class="btn btn-primary" href='../class_inserts/yoga.php' role="button">Yoga signup</a>
+    echo "</thead>";
+    foreach ($result as $rows) :
+        echo "<tbody>";
+        echo "<tr>";
+        echo "<td>" . $rows['class_id'] . "</td>";
+        echo "<td>" . $rows['trainer_first_name'] . "</td>";
+        echo "<td>" . $rows['class_name'] . "</td>";
+        echo "<td>" . $rows['class_time'] . "</td>";
+        echo "</tr>";
+        echo "</tbody>";
+    endforeach;
+    echo "</table>";
+    ?>
 
 
-<!-- 1. apply button using img to classes //coffeetime from cameron
+    <!-- 1. apply button using img to classes //coffeetime from cameron
 2. fix xml upload should be close -->
--- includes/classes_insert_inc.php
+++ includes/classes_insert_inc.php
@@ -3,29 +3,24 @@
 if (isset($_SESSION['user_id']) && !isset($_SESSION['admin'])) {
     $class_id = $_POST['class_id'];
     $user_id = $_POST['user_id'];
-    $classname = $_POST['class_name'];
-    $username = $_POST['username'];
 
     require_once "dbh.inc.php";
 
-    $query = "INSERT INTO bookings (class_id, user_id, class_name, username)
-    VALUES (:class_id, :user_id, :classname, :username)";
+    $query = "INSERT INTO bookings(class_id, user_id)
+    VALUES (:class_id, :user_id)";
 
     // Prepare the statement
     $stmt = $pdo->prepare($query);
 
     // Bind values to placeholders
-    $stmt->bindParam(':class_id', $class_id);
-    $stmt->bindParam(':user_id', $user_id);
-    $stmt->bindParam(':classname', $classname);
-    $stmt->bindParam(':username', $username);
-
+    $stmt->bindValue(':class_id', $class_id);
+    $stmt->bindValue(':user_id', $user_id);
 
     // Execute the statement
     $stmt->execute();
 
-    // Send the user back to the front page
     header("Location: ../pages/classes.php");
+
 
     // Close the connection and statement
     $pdo = null;
@@ -34,4 +29,4 @@ if (isset($_SESSION['user_id']) && !isset($_SESSION['admin'])) {
     die();
 } else {
     die();
-}
+};
